31c7a53c7cbcc97a7dbaecaff7ea3b766b4727cc
Brandon Miller <blmiller0809@gmail.com>
2024-09-20 13:29:45 -0500

    add filter to only show games on or after today, set game_id to None if there are no games at all

--- nhlcompanion/blueprints/teams.py
+++ nhlcompanion/blueprints/teams.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from flask import Blueprint, render_template, make_response
 from nhlcompanion.models import Teams, Schedule
-from sqlalchemy import or_
+from sqlalchemy import or_, and_
 
 bp = Blueprint('teams', __name__, url_prefix='/start')
 
@@ -20,21 +20,24 @@ def team_page(team):
 
     teams = Teams.query.all()
     team_schedule = Schedule.query.filter(or_(Schedule.home_team==team,
-                                              Schedule.away_team==team)).all()
+                                              Schedule.away_team==team), and_(Schedule.date <= today)).all()
     
-    for game in team_schedule:
-        if game.date == f'{today}':
-            game_id = game.game_id
-            break
-        else:
-            game_id = None
+    if len(team_schedule) > 0:
+        for game in team_schedule:
+            if game.date == f'{today}':
+                game_id = game.game_id
+                break
+            else:
+                game_id = None
+    else:
+        game_id = None
 
     # SET TEAM DATA VALUES IN COOKIES FOR EASY ACCESS
     res = make_response(render_template('app/team_page.html',
                            teams = teams,
                            team_schedule = team_schedule,
                            team_abbreviation = team,
-                           game_id = game_id
+                           game_id = game_id if game_id else None
                            ))
     res.set_cookie('team', team)
     

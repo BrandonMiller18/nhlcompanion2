5aa62965af23b276f1d6e91331d885e56b488fee
Brandon Miller <blmiller0809@gmail.com>
2024-10-28 23:18:17 -0500

    redesign, removed pages, ticket link

--- nhlcompanion/blueprints/teams.py
+++ nhlcompanion/blueprints/teams.py
@@ -6,37 +6,34 @@ from sqlalchemy import or_, asc
 
 bp = Blueprint('teams', __name__, url_prefix='/start')
 
-@bp.route('/')
-def pick_team():
-
-    teams = Teams.query.all()
-
-    return render_template('app/pick-team.html',
-                           teams = teams)
-
 @bp.route('/<team>')
 def team_page(team):
+    
+    teams = Teams.query.all()
+    
+    team_abbrs = []
+    for db_team in teams:
+        team_abbrs.append(db_team.team_abbreviation)
+        
+    if team not in team_abbrs:
+        return "404"
+    
     format = "%Y-%m-%d"
     today = datetime.datetime.combine(datetime.date.today(), datetime.datetime.min.time())
-    print(type(today))
-
-    teams = Teams.query.all()
+    
     team_schedule = Schedule.query.filter(or_(Schedule.home_team==team,
                                               Schedule.away_team==team)).order_by(asc(Schedule.date)).all()
-    
-    
+
     game_id = None
     if len(team_schedule) > 0:
         for game in team_schedule:
             game.date = datetime.datetime.strptime(game.date, format)
-            print(type(game.date))
+            
             if game.date == today:
                 game_id = game.game_id
-                
-
 
     # SET TEAM DATA VALUES IN COOKIES FOR EASY ACCESS
-    res = make_response(render_template('app/test_team_page.html',
+    res = make_response(render_template('app/team_page.html',
                            today = today,
                            teams = teams,
                            team_schedule = team_schedule,

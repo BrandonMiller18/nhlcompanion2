d5a63b464f7e92ec060e8942a6e2257b0cdf6e5d
Brandon Miller <blmiller0809@gmail.com>
2024-11-09 00:11:55 -0600

    migrate to plays array, trigger event when goal scores

--- nhlcompanion/static/js/watchgame.js
+++ nhlcompanion/static/js/watchgame.js
@@ -1,11 +1,16 @@
 let slug = (url) => new URL(url).pathname.match(/[^\/]+/g);
-var game_id = slug(document.location.href)[2];
+var gameId = slug(document.location.href)[2];
 
 // set variables from cookies
 var gameIdMeta = getCookie("nhlc_game_id")
 var userTeam = getCookie("nhlc_team");
 var streamDelay = getCookie("nhlc_stream_delay");
 var webhook = getCookie("nhlc_webhook");
+var homeTeamLogo = getCookie("nhlc_homeTeamLogo");
+var awayTeamLogo = getCookie("nhlc_awayTeamLogo");
+var homeTeamId = getCookie("nhlc_homeTeamId");
+var awayTeamId = getCookie("nhlc_awayTeamId");
+var userTeamId = getCookie("nhlc_userTeamId");
 
 // set the goal horn file
 var goalHorn = new Audio('/static/sounds/' + userTeam.toLowerCase() + '.mp3');
@@ -16,137 +21,172 @@ var interval = 3000;
 // set game over to false. If game is over, this will change to true and prevent more calls
 var gameOver = false;
 
-// KNOWN BUG WITH INTERVAL - on the first call, the interval is always 3 seconds, so no matter the state the call will be made twice
-// this leads to an extra call even if the game is over or not started yet. 
+function getGameData(callback) {
 
-function getGameData() {
-    let gameData = $.ajax({
-        type: "POST",
-        url: "/watch-game/_update-score/" + game_id,
-        success: function () {
-            // if game over is false, run these functions, if true, do nothing
-            if (!gameOver) {
-                setTimeout(function () {
-                    let newGameData = getGameData();
-                    updateScore(newGameData);
-                }, interval); // run this function at the determined interval based on game state
-            } else {
-                console.log("NHL Companion: game is over")
-            };
+    $.ajax({
+        type: "get",
+        url: "/watch-game/_update-score/" + gameId,
+        dataType: "json",
+        success: function (data) {
+            callback(data)
         },
-        async: false
+        error: function (err) {
+            console.log(err);
+        }
     });
 
-    let res = gameData.responseJSON;
-
-    return res;
 };
 
-function updateScore(gameData) {
 
-    // if game is not live, break function, do not send AJAX call, set scores
-    if (gameData) {
-        const liveGameStates = ["LIVE", "CRIT"];
-        if (!(liveGameStates.includes(gameData.gameState))) {
-            $('#home-score').html(gameData.homeTeam.score);
-            $('#away-score').html(gameData.awayTeam.score);
-            $('#period-label').hide();
-            if (gameData.gameState == "FUT") {
-                $('#period').html("GAME NOT STARTED<br>Data is automatically refreshed every 30 minutes.");
-                $('#time-div').hide();
-                // if not started, check API every 30 minutes
-                interval = 1800000;
-            } else if (gameData.gameState == "PRE") {
-                $('#period').html("GAME ABOUT TO START<br>Data is automatically refreshed every 2 minutes.");
-                // if pregame, check API every 2 minutes
-                interval = 120000;
-            } else {
-                $('#period').html("GAME OVER");
-                $('#time-div').hide();
-                gameOver = true;
-                return;
-            };
-            $('#time-div').hide();
-        } else {
-            $('#period-label').show();
-            $('#time-div').show();
-        };
-    };
+function showToast(playType, player, playerHeadshot, teamLogo) {
+    const toast = document.getElementById('toast');
 
-    // find current score as displayed on the page, this value is updated
-    // if the new score is differnet... initial value is "Getting scores..."
-    let homeScore = $('#home-score').html();
-    let awayScore = $('#away-score').html()
+    if (toast) {
+        toast.querySelector('.play-type').textContent = playType;
+        toast.querySelector('.player-name').textContent = player;
+        toast.querySelector('.player-headshot').src = playerHeadshot;
+        toast.querySelector('.team-logo-toast').src = teamLogo;
 
-    // set the new scores
-    let newHomeScore = JSON.parse(gameData.homeTeam.score);
-    let newAwayScore = JSON.parse(gameData.awayTeam.score);
+        // Show the toast with the 'show' class
+        toast.classList.add('show');
 
-    // get last period type of game returns str REG or OT (i think....)
-    gameData.gameOutcome ? lastPeriodType = gameData.gameOutcome.lastPeriodType : lastPeriodType = false;
+        //Fade out after 3 seconds
+        setTimeout(() => {
+            toast.classList.remove('show');
+        }, 3000);
+    }
+}
 
-    // get game state returns str: LIVE, OFF, FUT
-    let gameState = gameData.gameState;
 
-    // get intermission state returns true/false
+function setContent(gameData) {
+    const liveGameStates = ["LIVE", "CRIT"];
     let isIntermission = gameData.clock.inIntermission;
 
-    // get period to display
-    let displayPeriod = gameData.displayPeriod;
+    $('#home-score').html(gameData.homeTeam.score);
+    $('#away-score').html(gameData.awayTeam.score);
+    $('#time').html(gameData.clock.timeRemaining);
+    $('#period').html(gameData.displayPeriod);
+
+
+    if (!(liveGameStates.includes(gameData.gameState))) {
+
+        $('#period-label').hide();
+        if (gameData.gameState == "FUT") {
+            $('#period').html("GAME NOT STARTED<br>Data is automatically refreshed every 30 minutes.");
+            $('#time-div').hide();
+            // if not started, check API every 30 minutes
+            interval = 1800000;
+        } else if (gameData.gameState == "PRE") {
+            $('#period').html("GAME ABOUT TO START<br>Data is automatically refreshed every 2 minutes.");
+            // if pregame, check API every 2 minutes
+            interval = 120000;
+        } else {
+            $('#period').html("GAME OVER");
+            $('#time-div').hide();
+            gameOver = true;
+            return;
+        };
+        $('#time-div').hide();
+    } else {
+        $('#period-label').show();
+        $('#time-div').show();
+
+    };
 
     if (isIntermission) {
         // if intermission, check only every 2 minutes
         interval = 120000;
-        switch (displayPeriod) {
+        $('#period-label').hide();
+
+        switch (gameData.displayPeriod) {
             case 1:
                 $('#period').html("1st Intermission");
             case 2:
                 $('#period').html("2nd Intermission");
         }
-    } else if (gameData.gameState == "PRE" || gameData.gameState == "FUT") {
-        console.log("game not started")
     } else {
-        interval = 3000;
-        $('#period').html(displayPeriod);
-    };
+        $('#period-label').show();
+    }
 
-    // set the clock time 
-    $('#time').html(gameData.clock.timeRemaining);
-
-    // check if new score is different from old score
-    // if it is, change it -- also checks if user_team for goal horn
-    if (newHomeScore != homeScore) {
-        setTimeout(function () {
-            $('#home-score').html(newHomeScore);
-            if (newHomeScore > homeScore) {
-                if (userTeam == gameData.homeTeam.abbrev) {
-                    goalHorn.play();
-                    webhookRequest(webhook, null);
-                };
-            };
-        }, streamDelay);
-    };
-
-    if (newAwayScore != awayScore) {
-        setTimeout(function () {
-            $('#away-score').html(newAwayScore);
-            if (newAwayScore > awayScore) {
-                if (userTeam == gameData.awayTeam.abbrev) {
-                    goalHorn.play();
-                    webhookRequest(webhook, null);
-                };
-            };
-        }, streamDelay);
-    };
-
-    console.log("NHL Companion: checking every: " + interval / 1000 + " seconds");
 }
 
-// when document loads, get the initial game data then run the updateScore function
-$(document).ready(function () {
-    // get initial data to set game state, etc...
-    let gameData = getGameData();
 
-    // call function to continually update scores
-    updateScore(gameData);
-}); 
+function evaluatePlay(play, gameData) {
+    console.log(play.typeDescKey);
+    if (play.typeDescKey == "goal") {
+        var scoringPlayerId = play.details.scoringPlayerId;
+        console.log(scoringPlayerId);
+        var scoringPlayerTotal = play.details.scoringPlayerTotal;
+
+        var primaryAssistPlayerId = play.details.assist1PlayerId;
+        var secondaryAssistPlayerId = play.details.assist2PlayerId;
+        var scoringTeamId = play.details.eventOwnerTeamId;
+
+        var newHomeScore = play.details.homeScore;
+        var newAwayScore = play.details.awayScore;
+
+
+        for (i = 0; i < gameData.rosterSpots.length; i++) {
+            if (gameData.rosterSpots[i].playerId == scoringPlayerId) {
+                var playerFirstName = gameData.rosterSpots[i].firstName.default;
+                var playerLastName = gameData.rosterSpots[i].lastName.default;
+                var playerNumber = gameData.rosterSpots[i].sweaterNumber.default;
+                var playerHeadshot = gameData.rosterSpots[i].headshot;
+                var playerTeamId = gameData.rosterSpots[i].teamId;
+                if (playerTeamId == homeTeamId) {
+                    var displayLogo = homeTeamLogo
+                } else {
+                    var displayLogo = awayTeamLogo
+                }
+                console.log(playerHeadshot);
+                showToast('Goal', playerFirstName + ' ' + playerLastName, playerHeadshot, displayLogo);
+            };
+        };
+
+
+        if (scoringTeamId == userTeamId) {
+            webhookRequest(webhook, null);
+            goalHorn.play();
+        }
+
+
+    };
+}
+
+
+async function watchGame(gameData) {
+    const liveGameStates = ["LIVE", "CRIT"];
+
+    var seenPlayIds = []
+    for (i = 0; i < gameData.plays.length; i++) {
+        let eventId = gameData.plays[i].eventId;
+        seenPlayIds.push(eventId);
+    }
+
+    while (liveGameStates.includes(gameData.gameState)) {
+        await new Promise(r => setTimeout(r, streamDelay * 1000));
+
+        getGameData(function (res) {
+            console.log(res);
+            setContent(res);
+
+            var plays = res.plays
+
+            for (i = 0; i < plays.length; i++) {
+                let playId = plays[i].eventId;
+                if (!(seenPlayIds.includes(playId))) {
+                    evaluatePlay(plays[i], gameData);
+                    seenPlayIds.push(playId);
+                };
+            };
+        });
+    };
+};
+
+
+$(document).ready(function () {
+    getGameData(function (gameData) {
+        setContent(gameData);
+        watchGame(gameData);
+    });
+});
